4665e3e566c8b99f08bea26284790c7ebcdcb081
Emil <972080809@qq.com>
2019-08-21 12:00:06 +0800

    Hexo外链插件

---
+++ index.js
@@ -0,0 +1,34 @@
+const cheerio = require('cheerio');
+const Base64 = require('./lib/base64');
+const config = hexo.config.hexo_external_link = Object.assign({
+    enable: false,
+    enable_base64_encode: false,
+    url_param_name: 'u',
+    html_file_name: 'go.html',
+}, hexo.config.hexo_external_link);
+const domain = hexo.config.domain;
+const root = hexo.config.root || '/';
+const UrlReg = '^((http|https|thunder|qqdl|ed2k|Flashget|qbrowser|ftp|rtsp|mms)://)';
+if (config.enable) {
+    hexo.extend.filter.register('before_generate', function () {
+        hexo.extend.filter.register('after_render:html', data => {
+            const $ = cheerio.load(data);
+            $('a').each(function (i, elem) {
+                const href = $(elem).attr('href');
+                if (href && href.match(UrlReg)) {
+                    const strs = href.split('/');
+                    if (strs.length >= 3) {
+                        const host = strs[2];
+                        if (host !== domain) {
+                            $(elem).attr('href', `${root}${config.html_file_name}?${config.url_param_name}=${config.enable_base64_encode ? Base64.encode(href) : href}`)
+                                .attr('rel', 'external nofollow noopener noreferrer')
+                                .attr('target', '_blank');
+                        }
+                    }
+                }
+            });
+            return $.html();
+        })
+    });
+    hexo.extend.generator.register('external_link', require('./lib/generator'));
+}
